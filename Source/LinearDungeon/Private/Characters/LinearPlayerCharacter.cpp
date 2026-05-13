34c2e0e9b1d8733d15e8d326dad266f7c878748e
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-05-13 21:04:25 +0900

    LinearPlayerCharacter を用意して、カメラとSpringArmを持たせた。

---
+++ Source/LinearDungeon/Private/Characters/LinearPlayerCharacter.cpp
@@ -0,0 +1,20 @@
+﻿#include "Characters/LinearPlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ALinearPlayerCharacter::ALinearPlayerCharacter()
+{
+	// Controller の回転に Character 自身は依存しないことを明示する
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmBlankCharacter"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 200.f;
+	SpringArm->bUsePawnControlRotation = true; // Controler の回転を SpringArmに反映させる
+	SpringArm->SetRelativeRotation(FRotator(-20.f, 0.f, 0.f));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
---
+++ Source/LinearDungeon/Public/Characters/LinearPlayerCharacter.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/LinearCharacterBase.h"
+
+#include "LinearPlayerCharacter.generated.h"
+
+// カメラ
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class LINEARDUNGEON_API ALinearPlayerCharacter : public ALinearCharacterBase
+{
+	GENERATED_BODY()
+public:
+	ALinearPlayerCharacter();
+private:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+
+};
