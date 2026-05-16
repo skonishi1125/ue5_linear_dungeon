7c593057cf4e7cf9d42cd845230813926ddf1539
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-05-16 17:09:52 +0900

    Paladin の ABP 用意

---
+++ Source/LinearDungeon/Private/Characters/LinearPlayerAnimInstance.cpp
@@ -0,0 +1,25 @@
+﻿#include "Characters/LinearPlayerAnimInstance.h"
+#include "Characters/LinearPlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h" // GroundSpeed 取得用
+
+void ULinearPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	LinearPlayerCharacter = Cast<ALinearPlayerCharacter>(TryGetPawnOwner());
+	if (LinearPlayerCharacter)
+	{
+		LinearPlayerCharacterMovement = LinearPlayerCharacter->GetCharacterMovement();
+	}
+}
+
+void ULinearPlayerAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (LinearPlayerCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(LinearPlayerCharacterMovement->Velocity);
+	}
+}
---
+++ Source/LinearDungeon/Public/Characters/LinearPlayerAnimInstance.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "LinearPlayerAnimInstance.generated.h"
+
+class ALinearPlayerCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class LINEARDUNGEON_API ULinearPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ALinearPlayerCharacter> LinearPlayerCharacter;
+
+	UPROPERTY(BlueprintReadOnly, category = Movement)
+	TObjectPtr<UCharacterMovementComponent> LinearPlayerCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, category = Movement)
+	float GroundSpeed;
+
+};
