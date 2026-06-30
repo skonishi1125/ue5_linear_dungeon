6189da3d325f019b33867661aa1d40cb4398787a
skonishi <skonishi1125@gmail.com>
2026-06-30 10:06:16 +0900

    wip TitlePlayerController に、IMC / IA 追加(現時点では、まだ動かない)

--- Source/LinearDungeon/Private/Controllers/LinearTitlePlayerController.cpp
+++ Source/LinearDungeon/Private/Controllers/LinearTitlePlayerController.cpp
@@ -1,4 +1,14 @@
 #include "Controllers/LinearTitlePlayerController.h"
+#include "Logging/StructuredLog.h"
+
+// UI Navigation のキー操作調整用
+#include "Framework/Application/NavigationConfig.h"
+#include "Framework/Application/SlateApplication.h"
+
+// Enhanced Input
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
 
 void ALinearTitlePlayerController::BeginPlay()
 {
@@ -11,4 +21,38 @@ void ALinearTitlePlayerController::BeginPlay()
 
 	// マウスカーソルを表示
 	bShowMouseCursor = true;
+
+	// UMGナビゲーションキーのカスタマイズ
+	if (FSlateApplication::IsInitialized())
+	{
+		TSharedRef<FNavigationConfig> NavConfig = FSlateApplication::Get().GetNavigationConfig();
+		NavConfig->bKeyNavigation = true;
+
+		// 既存の設定にW/S/A/Dのルールを追加する
+		NavConfig->KeyEventRules.Emplace(EKeys::W, EUINavigation::Up);
+		NavConfig->KeyEventRules.Emplace(EKeys::S, EUINavigation::Down);
+		NavConfig->KeyEventRules.Emplace(EKeys::A, EUINavigation::Left);
+		NavConfig->KeyEventRules.Emplace(EKeys::D, EUINavigation::Right);
+	}
+
+}
+
+void ALinearTitlePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(CloseTitleSubMenuAction, ETriggerEvent::Started, this, &ALinearTitlePlayerController::CloseTitleSubMenu);
+	}
+	UE_LOGFMT(LogTemp, Warning, "ALinearTitlePlayerController::SetupInputComponent()");
+
+}
+
+void ALinearTitlePlayerController::CloseTitleSubMenu()
+{
+	// 現状効かないので、理由を探る。↑の SetupInputComponent は発火しているので、DefaultPawn などが問題だろうか？
+	UE_LOGFMT(LogTemp, Warning, "ALinearTitlePlayerController::CloseTitleSubMenu()");
+	
+	// TODO: Title の LoadMenu などのサブメニューを閉じる処理を書く
 }
--- Source/LinearDungeon/Public/Controllers/LinearTitlePlayerController.h
+++ Source/LinearDungeon/Public/Controllers/LinearTitlePlayerController.h
@@ -4,6 +4,11 @@
 #include "GameFramework/PlayerController.h"
 #include "LinearTitlePlayerController.generated.h"
 
+// Enhanced Input
+class UInputMappingContext;
+class UInputAction;
+class UEnhancedInputLocalPlayerSubsystem;
+
 UCLASS()
 class LINEARDUNGEON_API ALinearTitlePlayerController : public APlayerController
 {
@@ -11,5 +16,17 @@ class LINEARDUNGEON_API ALinearTitlePlayerController : public APlayerController
 
 protected:
 	virtual void BeginPlay() override;
+
+	// ===== Enhanced Input 関連 =====
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> TitleMenuMappingContext;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> CloseTitleSubMenuAction;
+	virtual void SetupInputComponent() override; // EnhancedInput Bindなど初期処理
+
+private:
+	// ===== メニュー開閉関連 =====
+	UFUNCTION()
+	void CloseTitleSubMenu();
 	
 };
